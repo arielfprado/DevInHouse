1eab3f3392a8c6ef3100a0812e2b9aac72b25ecc
arielfprado <arielfprado96@gmail.com>
2022-11-11 21:49:45 -0300

    aula5 commit

---
+++ javascript/aula5/exercicios/index.js
@@ -0,0 +1,27 @@
+function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      console.log(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  printNumbers(5, 10);
+
+  /* function printNumbers(from, to) {
+    let current = from;
+  
+    setTimeout(function go() {
+      alert(current);
+      if (current < to) {
+        setTimeout(go, 1000);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  printNumbers(5, 10); */
