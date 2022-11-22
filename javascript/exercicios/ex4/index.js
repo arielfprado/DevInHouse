a5ff1e6638cf68e95393f573cb031f10eb2e6a88
arielfprado <arielfprado96@gmail.com>
2022-11-22 11:37:53 -0300

    commit da resolução de todos os exercícios propostos

---
+++ javascript/exercicios/ex4/index.js
@@ -0,0 +1,18 @@
+let x = 17;
+let y = 21;
+let z = 42;
+
+//1
+console.log(x < y && y < z);
+
+//2
+console.log(x != z || y > z);
+
+//3
+console.log(z == 2*y);
+
+//4
+console.log(x + y > z);
+
+//5
+console.log(x**2 + y**2 > z**2);
